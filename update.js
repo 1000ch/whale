bbf08bec5fa690615d163ea4b0a291bd7f64bede
1000ch <shogo.sensui@gmail.com>
2017-08-05 20:16:47 +0900

    Tweaks

--- update.js
+++ update.js
@@ -1,34 +1,30 @@
 'use strict';
-const url = require('url');
 const electron = require('electron');
-
-const autoUpdater = electron.autoUpdater;
 
 module.exports.init = () => {
   if (process.platform !== 'darwin') {
     return;
   }
 
-  autoUpdater.on('checking-for-update', () => {
+  electron.autoUpdater.on('checking-for-update', () => {
     console.log('checking-for-update');
   });
 
-  autoUpdater.on('update-available', () => {
+  electron.autoUpdater.on('update-available', () => {
     console.log('update-available');
   });
 
-  autoUpdater.on('update-not-available', () => {
+  electron.autoUpdater.on('update-not-available', () => {
     console.log('update-not-available');
   });
 
-  autoUpdater.on('update-downloaded', () => {
+  electron.autoUpdater.on('update-downloaded', () => {
     console.log('update-downloaded');
   });
 
-  const { platform } = process;
   const version = electron.app.getVersion();
-  const feedURL = `https://hazel-mdyjxpqlnl.now.sh/update/${platform}/${version}`;
-  autoUpdater.setFeedURL(feedURL);
+  const feedURL = `https://hazel-mdyjxpqlnl.now.sh/update/${process.platform}/${version}`;
+  electron.autoUpdater.setFeedURL(feedURL);
 };
 
 module.exports.checkUpdate = () => {
@@ -36,5 +32,5 @@ module.exports.checkUpdate = () => {
     return;
   }
 
-  autoUpdater.checkForUpdates();
+  electron.autoUpdater.checkForUpdates();
 };
